af716a981620b54eee5d145d2a288e55dd61da6f
Thiago Meira <thiago5meira@outlook.com>
2023-10-28 11:49:17 -0300

    Criando o cabeçalho do bate papo

--- src/components/ChatContent.tsx
+++ src/components/ChatContent.tsx
@@ -1,10 +1,44 @@
-const ChatContent = () => {
+import React from 'react';
+import { BsFillTelephoneFill, BsFillCameraVideoFill } from 'react-icons/bs';
+import { BiSearch } from 'react-icons/bi';
+
+const ChatContent = ({ selectedContact }) => {
+  if (!selectedContact) {
+    return <div>Please select a contact to view messages.</div>;
+  }
 
   return (
-    <div>
-      testegwrghrqwhg
+    <div className='p-5 bg-gray-800 w-full border-b flex border-gray-600 '>
+      <div className='flex gap-4'>
+        <img src={selectedContact.img} width={70} alt="" />
+        <div className='flex flex-col justify-center'>
+          <span className='text-gray-300 text-xl font-bold'>{selectedContact.name}</span>
+          <span className='flex gap-2 items-center text-gray-300'>
+            <div className='w-2 h-2 rounded-full bg-emerald-400'></div>
+            Online
+          </span>
+        </div>
+      </div>
+      <div className='ml-auto flex items-center'>
+        <ul className='flex gap-3'>
+          <li className='
+            bg-gray-700 p-3 rounded-md text-gray-50 cursor-pointer
+            hover:text-violet-800 hover:bg-gray-300'>
+            <BsFillTelephoneFill />
+          </li>
+          <li className='
+            bg-gray-700 p-3 rounded-md text-gray-50 cursor-pointer
+            hover:text-violet-800 hover:bg-gray-300'>
+            <BsFillCameraVideoFill />
+          </li>
+          <li className='
+            bg-gray-700 p-3 rounded-md text-gray-50 cursor-pointer
+            hover:text-violet-800 hover:bg-gray-300'>
+            <BiSearch />
+          </li>
+        </ul> 
+      </div>
     </div>
   );
 };
-
 export default ChatContent
--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -1,28 +1,42 @@
+import React, { useState } from 'react';
 import { BiFilter, BiSearch  } from 'react-icons/bi';
 import ContactList from '../components/ContactList';
+import ChatContent from '../components/ChatContent';
 
 const Chat = () => {
+
+  const [selectedContact, setSelectedContact] = useState(null);
+
+  const handleContactSelect = (contact) => {
+    setSelectedContact(contact);
+  };
+
   return (
-    <div className="bg-gray-800 h-screen w-80 border-r border-gray-600 flex flex-col gap-4">
-      <ul className="flex justify-between px-4 pt-4 items-center">
-        <li className="text-white font-bold text-2xl flex items-center">Chats</li>
-        <li 
-          className="text-gray-400 font-bold text-sm flex items-center gap-2 cursor-pointer 
-          hover:text-violet-700">
-          <BiFilter size={20} /> 
-          Filter</li>
-      </ul>
-      <div className='relative flex px-4 items-center'>
-        <div className='absolute w-10 flex items-center justify-center'>
-          <BiSearch className='text-gray-400' />
+    <div className='flex w-full'>
+      <div className="bg-gray-800 h-screen w-80 border-r border-gray-600 flex flex-col gap-4">
+        <ul className="flex justify-between px-4 pt-4 items-center">
+          <li className="text-white font-bold text-2xl flex items-center">Chats</li>
+          <li 
+            className="text-gray-400 font-bold text-sm flex items-center gap-2 cursor-pointer 
+            hover:text-violet-700">
+            <BiFilter size={20} /> 
+            Filter</li>
+        </ul>
+        <div className='relative flex px-4 items-center'>
+          <div className='absolute w-10 flex items-center justify-center'>
+            <BiSearch className='text-gray-400' />
+          </div>
+          <input 
+            type="text"
+            placeholder='Procure pelo contato'
+            className='w-full p-2 pl-10 rounded-2xl bg-gray-800 border border-gray-600 text-sm text-gray-400 
+            focus:bg-gray-900 focus:outline-none focus:ring-0 focus:border-transparent'/>
         </div>
-        <input 
-          type="text"
-          placeholder='Procure pelo contato'
-          className='w-full p-2 pl-10 rounded-2xl bg-gray-800 border border-gray-600 text-sm text-gray-400 
-          focus:bg-gray-900 focus:outline-none focus:ring-0 focus:border-transparent'/>
+        <ContactList onSelectContact={handleContactSelect} />
       </div>
-      <ContactList />
+      <div className='w-full'>
+        <ChatContent selectedContact={selectedContact} />
+      </div>
     </div>
   )
 }
